796ab3a092695e51d721dbc3527d2693ff4b965d
iconjm <>
2023-11-10 12:44:32 +1100

    added cors URL, changed clerk publishableKey, addded title

--- client/social/src/main.tsx
+++ client/social/src/main.tsx
@@ -6,11 +6,9 @@ import './index.css'
 import { ClerkProvider } from "@clerk/clerk-react";
 import { PostsProvider } from './context/PostContext.tsx';
 
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
+    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
       <BrowserRouter>
         <PostsProvider>
           <App />
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const User = require('./models/userModel')
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://poemstack.onrender.com'],
     credentials: true
 }))
 
